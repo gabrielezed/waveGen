6f9c72b793d6fb26e6e2e51d14d8b020207497ad
gabrielezed <gabriele.pala00@outlook.it>
2026-03-20 08:42:49 +0100

    Added .gitignore and cleaned main.c

--- main.c
+++ main.c
@@ -15,15 +15,13 @@ int main(/*int argc, char* argv[]*/){
 	DoubleMatrix frequencies;
 	DoubleMatrix_init(&frequencies);
 	
-	printf("---Parsing test---\n");
-	
 	if(!parseJaggedFrequenciesFromFile("freqs.txt", &frequencies)){
 		fprintf(stderr, "Test failed: unable to open file\n");
 		DoubleMatrix_deep_free(&frequencies);
 		return 1;
 	}
 	
-	printf("---Parsing Jagged Array content---\n");
+	printf("--- Parsing Frequencies :3 ---\n");
 	for(size_t i = 0; i < frequencies.len; i++){
 		printf("Riga %zu (lunghezza %zu): ", i, frequencies.data[i].len);
 		for(size_t j = 0; j < frequencies.data[i].len; j++){
@@ -36,7 +34,7 @@ int main(/*int argc, char* argv[]*/){
 		fprintf(stderr, "No valid frequencies found in file.\n");
 	}
 	
-	int freqPerSec = 2;
+	int freqPerSec = 32;
 	int audioLenght = frequencies.len / freqPerSec;
 	if(audioLenght == 0) audioLenght = 1;
 	
@@ -44,39 +42,6 @@ int main(/*int argc, char* argv[]*/){
 	wavFileGen(&header, audioLenght, &frequencies, freqPerSec, AMPLITUDE);
 	
 	DoubleMatrix_deep_free(&frequencies);
-	
-	/*
-	//Wav generation test using data parsed from file
-	
-	DoubleArr frequencies;
-	DoubleArr_init(&frequencies);
-	
-	if(!parseFrequenciesFromFile("freqs.txt", &frequencies)){
-		DoubleArr_free(&frequencies);
-		return 1;
-	}
-	
-	if(frequencies.len == 0){
-		fprintf(stderr, "No valid frequencies found in file.\n");
-	}
-	
-	int freqPerSec = 2;
-	int audioLenght = frequencies.len / freqPerSec;
-	if(audioLenght == 0) audioLenght = 1;
-	
-	WavHeader header = createMonoHelper441_16(audioLenght);
-	wavFileGen(&header, audioLenght, frequencies.data, freqPerSec, AMPLITUDE);
-	
-	DoubleArr_free(&frequencies);
-	*/
-	
-	//Manual wav generation test
-	/*int audioLenght = 3;
-	int freqPerSec = 2;
-	double freq[6] = {600.0, 800.0, 400.0, 900.0, 600.0, 800.0};
-	WavHeader header = createMonoHelper441_16(audioLenght);
-	
-	wavFileGen(&header, audioLenght, freq, freqPerSec, AMPLITUDE);*/
 	
 	return 0;
 }
